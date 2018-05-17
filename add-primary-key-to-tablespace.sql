55487fbe844c592d3b0e490b303ebc823b331d1d
Shi Hao Wang <st890409@hotmail.com>
2018-05-17 15:24:49 +0800

    加入 PL/SQL 指令碼
    初始化測試資料與建立主索引鍵內容

---
+++ add-primary-key-to-tablespace.sql
@@ -0,0 +1,68 @@
+-- -----------------------------------------
+-- 依照不同情境建立主索引鍵 (primary-key)
+-- -----------------------------------------
+
+
+-- -----------------------------------------
+-- case 1 : 一般情況設定主索引鍵 (primary-key) 與索引 (index)
+-- -----------------------------------------
+CREATE INDEX SYSTEM.STOCK_SCHEMA_INDEX
+	ON SYSTEM.STOCKS (SCHEMA ASC);
+  
+ALTER TABLE SYSTEM.STOCKS ADD (
+	CONSTRAINT SYSTEM_STOCK_PK
+	PRIMARY KEY (NO)
+);
+
+/*
+DROP INDEX SYSTEM.STOCK_SCHEMA_INDEX;
+
+ALTER TABLE SYSTEM.STOCKS
+	DROP CONSTRAINT SYSTEM_STOCK_PK;
+*/
+
+-- -----------------------------------------
+-- case 2 : 將主索引鍵 (primary-key) 與索引 (index) 放到不同的表格空間 (tablespace)
+-- -----------------------------------------
+CREATE INDEX SYSTEM.STOCK_SCHEMA_INDEX
+	ON SYSTEM.STOCKS (SCHEMA ASC)
+	TABLESPACE STOCK_INDEX;
+  
+ALTER TABLE SYSTEM.STOCKS ADD (
+	CONSTRAINT SYSTEM_STOCK_PK
+	PRIMARY KEY (NO)
+	USING INDEX TABLESPACE STOCK_INDEX
+);
+
+/*
+DROP INDEX SYSTEM.STOCK_SCHEMA_INDEX;
+
+ALTER TABLE SYSTEM.STOCKS
+	DROP CONSTRAINT SYSTEM_STOCK_PK;
+*/
+
+-- -----------------------------------------
+-- case 2 : 先建立唯一索引後再建立主索引鍵 (primary-key)
+-- -----------------------------------------
+CREATE INDEX "SYSTEM".STOCK_SCHEMA_INDEX
+	ON SYSTEM.STOCKS (SCHEMA ASC)
+	TABLESPACE STOCK_INDEX;
+	
+CREATE UNIQUE INDEX "SYSTEM".STOCK_SCHEMA_PK
+	ON SYSTEM.STOCKS (NO ASC)
+	TABLESPACE STOCK_INDEX;
+
+ALTER TABLE "SYSTEM".STOCKS ADD (
+	CONSTRAINT STOCK_SCHEMA_PK
+	PRIMARY KEY(NO)
+	USING INDEX "SYSTEM"."STOCK_SCHEMA_PK"
+);
+
+/*
+DROP INDEX SYSTEM.STOCK_SCHEMA_INDEX;
+
+DROP INDEX SYSTEM.STOCK_SCHEMA_PK;
+
+ALTER TABLE SYSTEM.STOCKS
+	DROP CONSTRAINT SYSTEM_STOCK_PK;
+*/
